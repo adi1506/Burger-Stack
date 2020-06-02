d22ac57e9395269a4e65985b9cc9072f854b36d4
Aditya Chatterjee <adityachatterjee891@gmail.com>
2020-06-02 23:31:17 +0530

    commiting project skeleton

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Auxillary from '../../hoc/Auxillary';
+import classes from './Layout.module.css';
+
+const Layout = (props) => (
+   <Auxillary>
+      <div>Toolbar,SideDrawer,Backdrop</div>
+      <main className={classes.Content}>{props.children}</main>
+   </Auxillary>
+);
+
+export default Layout;
